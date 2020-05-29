5648fe1848acdcc4d200c0f1e705b22f2ad11eda
Kirill Iizumskii <mr.llirus@gmail.com>
2020-05-30 01:08:43 +0300

    API VK

--- Vk.py
+++ Vk.py
@@ -1,9 +1,10 @@
 from urllib.parse import urlencode
 from pprint import pprint
+import json
 
 import requests
 
-APP_ID = 7490139
+APP_ID = 7490365
 OAUTH_URL = 'https://oauth.vk.com/authorize'
 OAUTH_PARAMS = {
     'client_id': APP_ID,
@@ -13,14 +14,16 @@ OAUTH_PARAMS = {
     'v': '5.52'
 }
 
-TOKEN = '7370e3a5db05f0795fef94b584fa9c4c0b53537a09592f30b3b31f1255d42b00a080ced3a66ef2e44f0dd'
-TOKEN2 = 'cac8b520abee0fc12fd7ab519d452c261bd8cdc0220238a4ac8a80012db60a7dc4647d81dcbf329dd60f0'
+TOKEN = 'c67b8de203d401deeea696fd415c28fd2af1c9022f18a6c13d294c0fa866c065dd0c13ab5dacad3878139'
+TOKEN2 = 'eec04b1980aafb5d1ff96463af0ea1fd169dbf32cc15a7fd69ee950b416087f6820b28996cc13ee788dbd'
+
 
 class User:
 
     def __init__(self, token):
         self.token = token
 
+    friends = None
 
     def get_friends(self):
         response = requests.get(
@@ -46,16 +49,17 @@ class User:
         return response.json()
 
     def __and__(self, other):
-        pass
+        return list(set(self.friends) & set(other.friends))
 
-    def __str__(self):
-        return "https://vk.com/id{}".format()
 
-    friends = None
+#    def __str__(self):
+#        return "https://vk.com/id{}".format()
 
 
 user1 = User(TOKEN)
+user1.friends = user1.get_friends()["response"]["items"]
+
 user2 = User(TOKEN2)
-user2.friends = user2.get_friends()
-pprint(user2.friends)
-#print(user1 & user2)
+user2.friends = user2.get_friends()["response"]["items"]
+
+pprint(user1 & user2)
